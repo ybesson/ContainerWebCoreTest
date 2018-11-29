a2637d45d1addc5e49a36f44bb2e6283c4b54c36
Yoann Besson <yoann.besson@sb2.fr>
2018-11-29 13:31:57 +0100

    Test

---
+++ ContainerWebCoreTest/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ContainerWebCoreTest.Models;
+using ContainerWebCoreTest.Services;
+
+namespace ContainerWebCoreTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IMyService Service { get; }
+
+        public HomeController(IMyService service)
+        {
+            Service = service;
+        }
+
+        public IActionResult Index()
+        {
+            Service.DoWork();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ContainerWebCoreTest/Services/IMyService.cs
@@ -0,0 +1,27 @@
+﻿using StructureMap;
+
+namespace ContainerWebCoreTest.Services
+{
+    public interface IDummy { }
+    public class Dummy : IDummy { }
+
+    public interface IMyService
+    {
+        void DoWork();
+    }
+
+    public class MyService : IMyService
+    {
+        public IContainer Container { get; }
+
+        public MyService(IContainer container)
+        {
+            Container = container;
+        }
+
+        public void DoWork()
+        {
+            var dummy = Container.GetInstance<IDummy>();
+        }
+    }
+}
